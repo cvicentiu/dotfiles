b863186563648adb690f0ddff0f820231cb7b0de
Vicențiu Ciorbaru <cvicentiu@gmail.com>
2023-03-28 22:22:58 +0300

    Fix rose pine colors after update

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,31 +1,31 @@
 require('rose-pine').setup({
-	--- @usage 'main' | 'moon'
-	dark_variant = 'main',
-	bold_vert_split = false,
-	dim_nc_background = true,
-	disable_background = true,
-	disable_float_background = false,
-	disable_italics = false,
+    --- @usage 'main' | 'moon'
+    dark_variant = 'main',
+    bold_vert_split = false,
+    dim_nc_background = true,
+    disable_background = true,
+    disable_float_background = false,
+    disable_italics = false,
 
 
-	-- Change specific vim highlight groups
-	highlight_groups = {
+    -- Change specific vim highlight groups
+    highlight_groups = {
         -- TSProperty = { fg = p.iris, style = styles.italic },
-        TSVariable = { style = 'none' },
-		-- ColorColumn = { bg = 'surface' },
-        ['@variable'] = { style = 'none' },
+        -- TSVariable = { style = 'none' },
+        -- ColorColumn = { bg = 'surface' },
+        ['@variable'] = { italic = false },
 
-	}
+    }
 })
 
 -- set colorscheme after options
 vim.cmd('colorscheme rose-pine')
 function SetColorScheme(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
+    color = color or "rose-pine"
+    vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 SetColorScheme()
